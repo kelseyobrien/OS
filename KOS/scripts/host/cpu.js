b31a76ec23391116cb6e2b884c03de762ee059ea
kelseyobrien <kelsey.obrien524@gmail.com>
2013-10-02 18:42:12 -0400

    Forgot a file on my last push -___-

--- KOS/scripts/host/cpu.js
+++ KOS/scripts/host/cpu.js
@@ -34,5 +34,116 @@ function Cpu() {
         krnTrace("CPU cycle");
         // TODO: Accumulate CPU usage and profiling statistics here.
         // Do the real work here. Be sure to set this.isExecuting appropriately.
+	
+	this.fetch = function(){
+	}
+	
+	this.execute = function(code)
+	{
+		switch(code)
+		{
+			case "A9" : loadAccConst();
+			break;
+			case "AD" : loadAccMem();
+			break;
+			case "8D" : storeAccMem();
+			break
+			case "6D" : addWCarry();
+			break;
+			case "A2" : loadXWConst();
+			break;
+			case "AE" : loadXMem();
+			break;
+			case "A0" : loadYWConst();
+			break;
+			case "AC" : loadYMem();
+			break;
+			case "EA" : noOp();
+			break;
+			case "00" : breakSysCall();
+			break;
+			case "EC" : compToX();
+			break;
+			case "D0" : branchXBytes();
+			break;
+			case "EE" : incByte();
+			break;
+			case "FF" : sysCall();
+			break;
+			
+			
+		}
+	}
     };
+}
+
+// A9 = Load accumulator with constant
+function loadAccConst()
+{
+}
+
+//AD = Load accumulator from memory
+function loadAccMem()
+{
+}
+
+//8D = Store accumulator in memory
+function storeAccMem()
+{
+}
+
+//6D = Add with carry
+function addWCarry()
+{
+}
+
+//A2 = Load X register with constant
+function loadXWConst()
+{
+}
+
+//AE = Load X register from memory
+function loadXMem()
+{
+}
+
+//A0 = Load Y register with constant
+function loadYWConst()
+{
+}
+
+//AC = Load Y register from memory
+function loadYMem()
+{
+}
+
+//EA = No operation
+function noOp()
+{
+}
+
+//00 = break or system call
+function breakSysCall()
+{
+}
+
+//EC = compare byte in memory to X reg
+//	Z flag set to ) if equal
+function compToX()
+{
+}
+
+//D0 = Brank X bytes if Z flag = 0
+function branchXBytes()
+{
+}
+
+//EE = Increment value of a byte
+function incByte()
+{
+}
+
+//FF = System call
+function sysCall()
+{
 }
